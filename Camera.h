a1d8c63834102529ac4897be43549daa5afbb80b
plvzfq-rit <james_christian_ruiz@dlsu.edu.ph>
2025-09-06 18:43:23 +0800

    refactor: extract camera logic to Camera class

---
+++ Camera.h
@@ -0,0 +1,76 @@
+//
+// Created by James Christian on 06/09/2025.
+//
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "color.h"
+#include "Hittable.h"
+
+class Camera {
+public:
+    double ASPECT_RATIO = 1.0;
+    int IMAGE_WIDTH = 100;
+    void render(const Hittable& world) {
+        initialize();
+
+        std::cout << "P3\n" << IMAGE_WIDTH << " " << IMAGE_HEIGHT << "\n255\n";
+
+        for (int currentRow = 0; currentRow < IMAGE_HEIGHT; currentRow++) {
+            std::clog << "\rScanlines remaining: " << (IMAGE_HEIGHT - currentRow) << " " << std::flush;
+            for (int currentColumn = 0; currentColumn < IMAGE_WIDTH; currentColumn++) {
+                auto pixelCenter = FIRST_PIXEL_LOCATION + (currentColumn * HORIZONTAL_PIXEL_CHANGE) + (currentRow * VERTICAL_PIXEL_CHANGE);
+                auto rayDirection = pixelCenter - CAMERA_CENTER;
+                Ray r(CAMERA_CENTER, rayDirection);
+
+                Color pixelColor = rayColor(r, world);
+                writeColor(std::cout, pixelColor);
+            }
+        }
+
+        std::clog << "\rDone.                             \n";
+    }
+
+private:
+    int IMAGE_HEIGHT;
+    Point3D CAMERA_CENTER;
+    Point3D FIRST_PIXEL_LOCATION;
+    Vector3D HORIZONTAL_PIXEL_CHANGE;
+    Vector3D VERTICAL_PIXEL_CHANGE;
+
+    void initialize() {
+        IMAGE_HEIGHT = int(IMAGE_WIDTH / ASPECT_RATIO);
+        IMAGE_HEIGHT = (IMAGE_HEIGHT < 1) ? 1 : IMAGE_HEIGHT;
+        auto FOCAL_LENGTH = 1.0;
+        auto VIEWPORT_HEIGHT = 2.0;
+        auto VIEWPORT_WIDTH = VIEWPORT_HEIGHT * (double(IMAGE_WIDTH) / IMAGE_HEIGHT);
+        CAMERA_CENTER = Point3D(0,0,0);
+
+        auto VIEWPORT_HORIZONTAL = Vector3D(VIEWPORT_WIDTH, 0, 0);
+        auto VIEWPORT_VERTICAL = Vector3D(0, -VIEWPORT_HEIGHT, 0);
+
+        HORIZONTAL_PIXEL_CHANGE = VIEWPORT_HORIZONTAL / IMAGE_WIDTH;
+        VERTICAL_PIXEL_CHANGE = VIEWPORT_VERTICAL / IMAGE_HEIGHT;
+
+        auto VIEWPORT_UPPER_LEFT = CAMERA_CENTER -
+            Vector3D(0,0,FOCAL_LENGTH) - VIEWPORT_HORIZONTAL / 2 - VIEWPORT_VERTICAL / 2;
+
+        FIRST_PIXEL_LOCATION = VIEWPORT_UPPER_LEFT + 0.5 * (HORIZONTAL_PIXEL_CHANGE + VERTICAL_PIXEL_CHANGE);
+    }
+
+    Color rayColor(const Ray& ray, const Hittable& world) const {
+        HitRecord record;
+        if (world.hitInTimeRange(ray, Interval(0, INFTY), record)) {
+            return 0.5 * (record.normalVector + Color(1,1,1));
+        }
+
+        Vector3D unitDirection = normalize(ray.direction());
+        auto BLUE_WEIGHT = 0.5 * (unitDirection.y() + 1.0);
+        auto WHITE_WEIGHT = 1 - BLUE_WEIGHT;
+        auto WHITE = Color(1.0, 1.0, 1.0);
+        auto BLUE = Color(0.5, 0.7, 1.0);
+        return WHITE_WEIGHT * WHITE + BLUE_WEIGHT * BLUE;
+    }
+};
+#endif //CAMERA_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <iostream>
 
+#include "Camera.h"
 #include "color.h"
 #include "Hittable.h"
 #include "HittableList.h"
@@ -9,63 +10,17 @@
 #include "Ray.h"
 #include "Vector3D.h"
 
-Color rayColor(const Ray& ray, const Hittable& world) {
-    HitRecord record;
-    if (world.hitInTimeRange(ray, Interval(0, INFTY), record)) {
-        return 0.5 * (record.normalVector + Color(1,1,1));
-    }
-
-    Vector3D unitDirection = normalize(ray.direction());
-    auto BLUE_WEIGHT = 0.5 * (unitDirection.y() + 1.0);
-    auto WHITE_WEIGHT = 1 - BLUE_WEIGHT;
-    auto WHITE = Color(1.0, 1.0, 1.0);
-    auto BLUE = Color(0.5, 0.7, 1.0);
-    return WHITE_WEIGHT * WHITE + BLUE_WEIGHT * BLUE;
-}
-
 int main() {
-    auto ASPECT_RATIO = 16.0 / 9.0;
-    int IMAGE_WIDTH = 400;
-
-    int IMAGE_HEIGHT = int(IMAGE_WIDTH / ASPECT_RATIO);
-    IMAGE_HEIGHT = (IMAGE_HEIGHT < 1) ? 1 : IMAGE_HEIGHT;
-
     HittableList world;
 
     world.add(std::make_shared<Sphere>(Point3D(0,0,-1), 0.5));
     world.add(std::make_shared<Sphere>(Point3D(0,-100.5,-1), 100));
 
-    auto FOCAL_LENGTH = 1.0;
-    auto VIEWPORT_HEIGHT = 2.0;
-    auto VIEWPORT_WIDTH = VIEWPORT_HEIGHT * (double(IMAGE_WIDTH) / IMAGE_HEIGHT);
-    auto CAMERA_CENTER = Point3D(0,0,0);
+    Camera camera;
+    camera.ASPECT_RATIO = 16.0 / 9.0;
+    camera.IMAGE_WIDTH = 400;
 
-    auto VIEWPORT_HORIZONTAL = Vector3D(VIEWPORT_WIDTH, 0, 0);
-    auto VIEWPORT_VERTICAL = Vector3D(0, -VIEWPORT_HEIGHT, 0);
+    camera.render(world);
 
-    auto HORIZONTAL_PIXEL_CHANGE = VIEWPORT_HORIZONTAL / IMAGE_WIDTH;
-    auto VERTICAL_PIXEL_CHANGE = VIEWPORT_VERTICAL / IMAGE_HEIGHT;
-
-    auto VIEWPORT_UPPER_LEFT = CAMERA_CENTER -
-        Vector3D(0,0,FOCAL_LENGTH) - VIEWPORT_HORIZONTAL / 2 - VIEWPORT_VERTICAL / 2;
-
-    auto FIRST_PIXEL_LOCATION = VIEWPORT_UPPER_LEFT + 0.5 * (HORIZONTAL_PIXEL_CHANGE + VERTICAL_PIXEL_CHANGE);
-
-
-    std::cout << "P3\n" << IMAGE_WIDTH << " " << IMAGE_HEIGHT << "\n255\n";
-
-    for (int currentRow = 0; currentRow < IMAGE_HEIGHT; currentRow++) {
-        std::clog << "\rScanlines remaining: " << (IMAGE_HEIGHT - currentRow) << " " << std::flush;
-        for (int currentColumn = 0; currentColumn < IMAGE_WIDTH; currentColumn++) {
-            auto pixelCenter = FIRST_PIXEL_LOCATION + (currentColumn * HORIZONTAL_PIXEL_CHANGE) + (currentRow * VERTICAL_PIXEL_CHANGE);
-            auto rayDirection = pixelCenter - CAMERA_CENTER;
-            Ray r(CAMERA_CENTER, rayDirection);
-
-            Color pixelColor = rayColor(r, world);
-            writeColor(std::cout, pixelColor);
-        }
-    }
-
-    std::clog << "\rDone.                             \n";
     return 0;
 }
