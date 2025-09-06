8c1e30677c95e8d47b07b3b53cd5f1afd075fa00
plvzfq-rit <james_christian_ruiz@dlsu.edu.ph>
2025-09-06 18:22:13 +0800

    feat: create Interval class

---
+++ Interval.h
@@ -0,0 +1,36 @@
+//
+// Created by James Christian on 06/09/2025.
+//
+
+#ifndef INTERVAL_H
+#define INTERVAL_H
+
+#include "math_stuff.h"
+
+class Interval {
+public:
+    double min;
+    double max;
+
+    Interval() : min(+INFTY), max(-INFTY) {}
+    Interval(double min, double max): min(min), max(max) {}
+
+    double size() const {
+        return max - min;
+    }
+
+    bool contains (double x) const {
+        return min <= x && x <= max;
+    }
+
+    bool surrounds(double x) const {
+        return min < x && x < max;
+    }
+
+    static const Interval empty, universe;
+};
+
+const Interval Interval::empty = Interval(INFTY, -INFTY);
+const Interval Interval::universe = Interval(-INFTY, INFTY);
+
+#endif //INTERVAL_H
