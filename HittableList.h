e4affc16b0508e0c95ce78d25e03032408d7eb45
plvzfq-rit <james_christian_ruiz@dlsu.edu.ph>
2025-09-06 17:50:56 +0800

    feat: add a class for hittable list

---
+++ HittableList.h
@@ -0,0 +1,46 @@
+//
+// Created by James Christian on 06/09/2025.
+//
+
+#ifndef HITTABLELIST_H
+#define HITTABLELIST_H
+
+#include <memory>
+#include <vector>
+
+#include "Hittable.h"
+
+class HittableList : public Hittable {
+public:
+    std::vector<std::shared_ptr<Hittable>> objects;
+
+    HittableList() {}
+    HittableList(std::shared_ptr<Hittable> object) {
+        add(object);
+    }
+
+    void clear() {
+        objects.clear();
+    }
+
+    void add(std::shared_ptr<Hittable> object) {
+        objects.push_back(object);
+    }
+
+    bool hitInTimeRange(const Ray& ray, double minTime, double maxTime, HitRecord& record) const override {
+        HitRecord tempRecord;
+        bool hitAny = false;
+        auto closestSoFar = maxTime;
+        for (auto& object : objects) {
+            if (object->hitInTimeRange(ray, minTime, closestSoFar, tempRecord)) {
+                hitAny = true;
+                closestSoFar = tempRecord.time;
+                record = tempRecord;
+            }
+        }
+
+        return hitAny;
+    }
+};
+
+#endif //HITTABLELIST_H
