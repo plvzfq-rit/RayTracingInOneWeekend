c78726752010c04026202cd21f600352c1f3471a
plvzfq-rit <james_christian_ruiz@dlsu.edu.ph>
2025-09-06 18:46:37 +0800

    feat: add clamp function to Interval

--- Interval.h
+++ Interval.h
@@ -27,6 +27,12 @@ public:
         return min < x && x < max;
     }
 
+    double clamp(double x) const {
+        if (x < min) return min;
+        if (x > max) return max;
+        return x;
+    }
+
     static const Interval empty, universe;
 };
 
