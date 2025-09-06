de6404a1e3e358febc86d0ede4c554301993e090
plvzfq-rit <james_christian_ruiz@dlsu.edu.ph>
2025-09-06 18:45:30 +0800

    feat: add double randmizer

--- math_stuff.h
+++ math_stuff.h
@@ -5,12 +5,23 @@
 #ifndef MATH_STUFF_H
 #define MATH_STUFF_H
 #include <limits>
+#include <random>
 
 const double INFTY = std::numeric_limits<double>::infinity();
 const double PI = 3.1415926535897932385;
 
 inline double degreesToRadians(double degrees) {
     return degrees * PI / 180.0;
+}
+
+inline double randomDouble() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double randomDouble(double min, double max) {
+    return min + (max - min) * randomDouble();
 }
 
 #endif //MATH_STUFF_H
