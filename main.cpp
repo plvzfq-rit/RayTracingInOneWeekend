361b05abfb725d9d08e10f9e110c61817fbda17b
plvzfq-rit <james_christian_ruiz@dlsu.edu.ph>
2025-09-06 14:27:16 +0800

    feat: simple gradient ppm generator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int imageWidth = 256;
+    int imageHeight = 256;
+
+    std::cout << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
+
+    for (int j = 0; j < imageHeight; j++) {
+        for (int i = 0; i < imageWidth; i++) {
+            auto r = double(i) / (imageWidth - 1);
+            auto g = double(j) / (imageHeight - 1);
+            auto b = 0.0;
+
+            int ir = int(r * 255.999);
+            int ig = int(g * 255.999);
+            int ib = int(b * 255.999);
+
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
 }
